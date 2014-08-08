d850f7ab0a4a67af88d8606d2df787620a3ab213
miyohide <miyohide@gmail.com>
2014-08-08 21:47:37 +0900

    add attach file validation.

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -1,3 +1,7 @@
 class Experiment < ActiveRecord::Base
-   has_attached_file :dstat_data
+   has_attached_file :dstat_data,
+      presence: true,
+      content_type: { content_type: ["text/csv", "application/vnd.ms-excel"] },
+      size: { in: 0..1.megabytes }
+
 end
