57e1ad9aa1d709b9b163eb447be28faf400bacbc
miyohide <miyohide@gmail.com>
2014-08-21 22:49:13 +0900

    create experiments factory.

--- spec/factories/experiments.rb
+++ spec/factories/experiments.rb
@@ -3,7 +3,11 @@
 FactoryGirl.define do
   factory :experiment do
     experiment_at "2014-08-05 22:23:06"
-    ruby_type "MyString"
-    note "MyText"
+    ruby_type "CRuby"
+    note "Experiment note"
+    dstat_data_file_name "file_name"
+    dstat_data_content_type "text/csv"
+    dstat_data_file_size 1024*1024
+    dstat_data_updated_at "2014-08-05 22:23:06"
   end
 end
