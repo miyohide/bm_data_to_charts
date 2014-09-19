6c5372cf01cb6be6a853f055cc162cf7ce465c9e
miyohide <miyohide@gmail.com>
2014-09-20 00:28:29 +0900

    create method that calcurates sum(cpu0-7).

--- app/models/experiment_detail.rb
+++ app/models/experiment_detail.rb
@@ -1,3 +1,7 @@
 class ExperimentDetail < ActiveRecord::Base
   belongs_to :experiment
+
+  def total_cpu_data
+     self.cpu0 + self.cpu1 + self.cpu2 + self.cpu3 + self.cpu4 + self.cpu5 + self.cpu6 + self.cpu7
+  end
 end
